5c32ad369ba326f6107b54a1949af8f05c85ef28
Ahmed Eltokhy <elt0khy@protonmail.com>
2020-11-30 15:42:00 +0200

    Add solution for problem `A - Magnets` Link: https://codeforces.com/contest/344/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/344/submission/99989385

--- codeforces/344/A.cpp
+++ codeforces/344/A.cpp
@@ -2,16 +2,7 @@
 
 using namespace std;
 
-typedef long long ll;
-typedef vector<int> vi;
-typedef pair<int,int> pi;
-
-#define PB push_back
-#define MP make_pair
-#define F first
-#define S second
 #define tt(n) for (int i=0; i<n; i++)
-#define FOR(i,a,n) for (auto i=a; i!=n; i++)
 
 
 int main(int argc, char *argv[]) {
@@ -20,7 +11,8 @@ int main(int argc, char *argv[]) {
 	cout.tie(0);	
 	//freopen(".in", "r", stdin);
 	int n; cin >> n;
-	int l[n];	
+	
+	int l[n];
 	for(int i=0; i<n; i++) {
 		string str;
 		cin >> str;
@@ -28,15 +20,13 @@ int main(int argc, char *argv[]) {
 			l[i]=0;
 		else
 			l[i]=1;
-		if(i==n-1) {
-			int ctr=1;
-			for(int i=1; i<n; i++) {
-			if(l[i]!=l[i-1])
-				ctr++;
-			}
-			cout << ctr;
-		}
 	}
+	int ctr=1;
+	for(int i=1; i<n; i++) {
+		if(l[i]!=l[i-1])
+			ctr++;
+	}
+	cout << ctr;
 
 	return 0;
 }
