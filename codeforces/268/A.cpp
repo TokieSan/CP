1f1b880a46b9070a4dbbfab90b708cc71868b70d
Ahmed Eltokhy <elt0khy@protonmail.com>
2020-12-08 19:56:00 +0200

    Add solution for problem `A - Games` Link: https://codeforces.com/contest/268/problem/A Tags: brute force, *800 Ref: https://codeforces.com/contest/268/submission/100718873

--- codeforces/268/A.cpp
+++ codeforces/268/A.cpp
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-
 #define FOR(i,a,n) for (auto i=a; i!=n; i++)
 
 
@@ -10,7 +9,6 @@ int main(int argc, char *argv[]) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);	
-	 
 	int n; cin >> n;
 	int N[n][2];
 	FOR(i,0,n) {
@@ -19,7 +17,7 @@ int main(int argc, char *argv[]) {
 	int ctr=0;
 	for(int i=0; i<n; i++) {
 		for(int j=0; j<n; j++) {
-			if(N[i][0]==N[j][1]) {
+			if(N[i][0]==N[j][1] && i!=j) {
 				ctr++;
 			}
 		}
