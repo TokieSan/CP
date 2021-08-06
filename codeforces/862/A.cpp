9e8b9b150ff12364cc7e282db97ea08b47339d4a
Ahmed Eltokhy <elt0khy@protonmail.com>
2021-08-06 04:36:00 +0200

    Add solution for problem `A - Mahmoud and Ehab and the MEX` Link: https://codeforces.com/contest/862/problem/A Tags: greedy, implementation, *1000 Ref: https://codeforces.com/contest/862/submission/125008271

---
+++ codeforces/862/A.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef priority_queue<int> pq;
+
+#define pb push_back
+#define V vector
+#define mp make_pair
+#define F first
+#define S second
+#define tt(n) for (int i=0; i<n; i++)
+#define rtt(n) for (int i=n-1; i>=0; i--)
+#define FOR(i,a,n) for (auto i=a; i!=n; i++)
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define G(x) int x; cin >> x;
+#define GG(x,y) int x,y; cin >> x >> y;
+#define GGG(x,y,z) int x,y,z; cin >> x >> y >> z;
+#define tt2(nx,ny) \
+	for(int i=0; i<nx; i++) \
+		for(int j=0; j<ny; j++)	
+
+#define    PI         3.14159265358979
+#define    AREA(r)    (PI*(r)*(r))
+#define	   MOD	1000000007
+
+ll power(ll a, ll b){
+    ll result=1;
+    while(b){
+        if(b%2) result*=a;
+        a*=a;
+        b/=2;
+    }		
+    return result;
+}
+
+//Euler Divide
+ll div(ll &a, ll &b, ll &P){
+	return a*power(b, P-2)%P;
+}
+template <class T> 
+bool sortbysec(const pair<T,T> &a, const pair<T,T> &b){
+	return (a.second < b.second);
+}
+bool isFraction(double &a){
+	if(a==static_cast<int>(a))	return true;
+	else return false;
+}
+bool validLimits(int x, int y, int nx, int ny) {
+	if(
+			x<0 ||
+			x>=nx ||
+			y<0 ||
+			y>=ny 
+		) return false;
+	return true;
+	
+}
+ll add(ll x, ll y) {
+	x += y;
+	if (x >= MOD) return x - MOD;
+	return x;
+}
+ll sub(ll x, ll y) {
+	x -= y;
+	if (x < 0) return x + MOD;
+	return x;
+}
+ll mul(ll x, ll y) {	
+	return (x * y) % MOD;
+}
+
+template <class T>
+void print(V<T> &a){
+	FOR(i, a.begin(), a.end())
+		cout << *i << ' ';
+	cout << '\n';
+}
+
+int main(int argc, char *argv[]) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);	
+	//freopen(".in", "r", stdin);
+	//freopen(".out", "w", stdout);
+	
+	GG(n,x);
+	map<int,int>M;
+	tt(n){
+		G(t); M[t]++;
+	}
+	int ans=0;
+	for(int i=0; i<x; i++){
+		if(!M[i]) ans++;
+	}
+	if(M[x])ans++;
+	cout << ans;
+
+	return 0;
+}
